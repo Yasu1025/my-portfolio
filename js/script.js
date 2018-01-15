d4ce715e1cae60a60a0ba8f5bd23dadbb20e48c5
Yasu <[sillyjp25@gmail.com]>
2018-01-16 04:15:57 +0900

    up

--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
 $(document).ready(function(){
+    // Load page
+     $('.slide01').click(function(){
+        $('.work_contents').load('pages/work.html')
+    })
+
+    
+
+
+    
+    // Slider
 
     var slideWidth = $('.slide').outerWidth();
     var slideNum = $('.slide').length;
     var allSlideWidth = slideWidth * slideNum;
     var current = 0;
 
+    $(window).resize(function(){
+        slideWidth = $('.slide').outerWidth();
+        allSlideWidth = slideWidth * slideNum;
+        $('.slide_container  .slide').css({'width': "calc(100vw - 12vmax)", "height": "calc(100vw - 12vmax);"})
+        $('.slideset').css({"width": allSlideWidth})
+    })
+
     $('.slideset').css({"width": allSlideWidth})
+
+    var navArrowshowHide = function(){
+        if(current === 0){
+            $('.home_arrow_prev').hide();
+        }else{
+            $('.home_arrow_prev').show();
+        }
+    }
+
 
     var sliding = function(){
         if(current < 0){
             current = slideNum -1;
         }else if(current > slideNum -1){
             current = 0;
+            navArrowshowHide();
         }
         $('.slideset').stop().animate({
             'left': current * -slideWidth
@@ -20,11 +47,14 @@ $(document).ready(function(){
 
     $('.home_arrow_prev').click(function(){
         current--;
+        navArrowshowHide();
         sliding();
     })
     $('.home_arrow_next').click(function(){
         current++;
+        navArrowshowHide();
         sliding();
     })
+
     
 });
